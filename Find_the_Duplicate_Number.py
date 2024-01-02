64b25c3067ef47f34ac683f0f5ba12d84653a9cb
SujayJakka <101142167+SujayJakka@users.noreply.github.com>
2024-01-02 17:34:17 -0600

    Create Find_the_Duplicate_Number.py

---
+++ Find_the_Duplicate_Number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        hare, turtle = 0, 0
+
+        while True:
+            hare = nums[hare]
+
+            turtle = nums[nums[turtle]]
+
+            if hare == turtle:
+                break
+
+        hare = 0
+        
+        while True:
+            hare = nums[hare]
+            turtle = nums[turtle]
+
+            if hare == turtle:
+                return turtle
