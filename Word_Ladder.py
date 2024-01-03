17244936167579d10e9c4bfbd9893be8aed66ef2
SujayJakka <101142167+SujayJakka@users.noreply.github.com>
2024-01-02 18:52:08 -0600

    Create Word_Ladder.py

---
+++ Word_Ladder.py
@@ -0,0 +1,30 @@
+class Solution:
+      
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+        l = len(beginWord)
+       
+        wordList.append(beginWord)
+        mapThing = collections.defaultdict(list)
+
+        for word in wordList:
+            for i in range(l):
+                mapThing[word[:i] + "*" + word[i+1:]].append(word)
+        
+        myQueue = [[beginWord, 1]]
+        visited = set(beginWord)
+
+        while myQueue:
+            word, res = myQueue.pop(0)
+            for i in range(l):
+                intermediate = word[:i] + "*" + word[i+1:]
+                closeWords = mapThing[intermediate]
+
+                for closeWord in closeWords:
+                    if closeWord == endWord:
+                        return res + 1
+                    if closeWord not in visited:
+                        myQueue.append([closeWord, res + 1])
+                        visited.add(closeWord)
+
+        return 0
