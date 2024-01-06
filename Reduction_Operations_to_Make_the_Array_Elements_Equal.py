2169ff0e2e4420a7b116c6ab71ab3193e199f0d6
SujayJakka <101142167+SujayJakka@users.noreply.github.com>
2024-01-05 23:54:14 -0600

    Create Reduction_Operations_to_Make_the_Array_Elements_Equal.py

---
+++ Reduction_Operations_to_Make_the_Array_Elements_Equal.py
@@ -0,0 +1,16 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        
+        nums.sort()
+        res = 0
+        operations = 0
+
+        for i in range(0, len(nums) - 1):
+            if nums[i] != nums[i + 1]:
+                operations += 1
+            res += operations
+
+        return res
